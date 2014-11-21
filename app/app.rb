484d47bc89dd1a1e3211a74037033f2862b7b2cf
Isaac Chansky <ichansky@gmail.com>
2014-11-21 00:10:17 -0500

    adhere to ruby style conventions

--- app/app.rb
+++ app/app.rb
@@ -3,26 +3,26 @@
 # A public community calendar aggregator JSON API
 # v0.0.1
 
-require 'sinatra/base'
-require 'sinatra/config_file'
-require 'json'
-require 'dotenv'
+require "sinatra/base"
+require "sinatra/config_file"
+require "json"
+require "dotenv"
 Dotenv.load
 
-require_relative 'models'
+require "./app/models"
 
 class Agora < Sinatra::Application
   register Sinatra::ConfigFile
   config_file("config.yml")
 
   # Set up DB
-  DataMapper.setup(:default, ENV['DATABASE_URL'] || "#{ENV['DB_ADAPTER']}://#{ENV['DB_HOSTNAME']}/#{ENV['DB_NAME']}")
+  DataMapper.setup(:default, ENV["DATABASE_URL"] || "#{ENV["DB_ADAPTER"]}://#{ENV["DB_HOSTNAME"]}/#{ENV["DB_NAME"]}")
 
   Event.auto_upgrade!
   Source.auto_upgrade!
 
   DataMapper.finalize
 
-  require './app/routes'
+  require "./app/routes"
 
 end
