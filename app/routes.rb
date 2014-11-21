8eb913b20466fa30bf335041366fb183130c27e1
Isaac Chansky <ichansky@gmail.com>
2014-11-20 23:40:48 -0500

    add sinatra namespace to version API. all api routes should be prefixed with 'api/v1'.

--- app/routes.rb
+++ app/routes.rb
@@ -1,6 +1,7 @@
-require 'slim'
+require "sinatra/namespace"
 require 'data_mapper'
 require './app/view_helpers'
+require 'slim'
 
 class Agora < Sinatra::Application
 
@@ -21,86 +22,94 @@ class Agora < Sinatra::Application
     halt 404, { error: 'URL is not found' }.to_json
   end
 
-  ####################
-  # EVENTS
-  ####################
-  get '/events' do
-    content_type :json
-    halt 200, Event.all.to_json
-  end
 
+  ########################################
+  # API V-1.0
+  ########################################
+  namespace '/api/v1' do
 
-  get '/events/:id' do
-    content_type :json
-    event = Event.get(params[:id])
-    if event
-      halt 200, event.to_json
-    else
-      halt 404, { :message => "Event not found" }.to_json
-    end
-  end
-
-
-  ####################
-  # Source
-  ####################
-  get '/sources' do
-    content_type :json
-    halt 200, Source.all.to_json
-  end
-
-  post '/sources' do
-    content_type :json
-    source = Source.new(JSON.parse(request.body.read))
-    begin
-      source.save
-      halt 201, source.to_json
-    rescue DataMapper::SaveFailureError => e
-      errors = e.resource.errors.full_messages
-      halt 400, { :errors => errors }.to_json
-    end
-  end
-
-  get '/sources/:id' do
-    content_type :json
-    source = Source.get(params[:id])
-    if source
-      halt 200, source.to_json
-    else
-      halt 404, { :message => "Source not found" }.to_json
-    end
-  end
-
-  put '/sources/:id' do
-    content_type :json
-    source = Source.get(params[:id])
-    if source == nil
-      halt 404, { :message => "Source not found" }.to_json
+    ####################
+    # EVENTS
+    ####################
+    get '/events' do
+      content_type :json
+      halt 200, Event.all.to_json
     end
 
-    begin
-      source.update(JSON.parse(request.body.read))
-      halt 200, source.to_json
-    rescue DataMapper::SaveFailureError => e
-      errors = e.resource.errors.full_messages
-      halt 400, { :errors => errors }.to_json
+
+    get '/events/:id' do
+      content_type :json
+      event = Event.get(params[:id])
+      if event
+        halt 200, event.to_json
+      else
+        halt 404, { :message => "Event not found" }.to_json
+      end
     end
+
+
+    ####################
+    # Source
+    ####################
+    get '/sources' do
+      content_type :json
+      halt 200, Source.all.to_json
+    end
+
+    post '/sources' do
+      content_type :json
+      source = Source.new(JSON.parse(request.body.read))
+      begin
+        source.save
+        halt 201, source.to_json
+      rescue DataMapper::SaveFailureError => e
+        errors = e.resource.errors.full_messages
+        halt 400, { :errors => errors }.to_json
+      end
+    end
+
+    get '/sources/:id' do
+      content_type :json
+      source = Source.get(params[:id])
+      if source
+        halt 200, source.to_json
+      else
+        halt 404, { :message => "Source not found" }.to_json
+      end
+    end
+
+    put '/sources/:id' do
+      content_type :json
+      source = Source.get(params[:id])
+      if source == nil
+        halt 404, { :message => "Source not found" }.to_json
+      end
+
+      begin
+        source.update(JSON.parse(request.body.read))
+        halt 200, source.to_json
+      rescue DataMapper::SaveFailureError => e
+        errors = e.resource.errors.full_messages
+        halt 400, { :errors => errors }.to_json
+      end
+    end
+
+    delete '/sources/:id' do
+      content_type :json
+      source = Source.get(params[:id])
+      if source == nil
+        halt 404, { :message => "Source not found" }.to_json
+      end
+
+      begin
+        source.destroy
+        halt 204, {}.to_json
+      rescue DataMapper::SaveFailureError => e
+        errors = e.resource.errors.full_messages
+        halt 400, { :errors => errors }.to_json
+      end
+    end
+
+  # end namespace
   end
-
-  delete '/sources/:id' do
-    content_type :json
-    source = Source.get(params[:id])
-    if source == nil
-      halt 404, { :message => "Source not found" }.to_json
-    end
-
-    begin
-      source.destroy
-      halt 204, {}.to_json
-    rescue DataMapper::SaveFailureError => e
-      errors = e.resource.errors.full_messages
-      halt 400, { :errors => errors }.to_json
-    end
-  end
-
 end
